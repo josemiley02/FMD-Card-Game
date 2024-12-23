ebcfeb9ed69974840b5a67e395bff6c3ee921c29
Miguel <leyvadelacruzjosemiguel20@gmail.com>
2024-12-23 18:25:54 -0500

    Implemented basic steps Game

--- Backend/Elements/Games/Game.cs
+++ Backend/Elements/Games/Game.cs
@@ -5,8 +5,20 @@ namespace FMD.Backend.Elements.Games;
 
 public class Game
 {
-    public Player Player1 { get; set; } = null!;
-    public Player Player2 { get; set; } = null!;
+    public Player CurrentPlayer { get; set; } = null!;
+    public Player OpponentPlayer { get; set; } = null!;
     public Card[,] Table { get; set; } = new Card[4, 3]; // 4 rows, 3 columns
     public List<Card> Graveyard { get; set; } = new List<Card>();
+
+    public void ChangeTurn()
+    {
+        var aux = CurrentPlayer;
+        CurrentPlayer = OpponentPlayer;
+        OpponentPlayer = aux;
+        PlayTurn();
+    }
+    public void PlayTurn()
+    {
+        
+    }
 }
